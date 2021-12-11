31d4428c9febfa352e2d371e5f2ba6d203dfa42a
raasalgul <k.sathishrsurya@gmail.com>
2021-12-11 13:38:27 +0000

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,3 +1,20 @@
+import { Grid } from "@material-ui/core";
+
 export default function About(){
-    return(<h1>About</h1>)
+    return(<Grid>
+        <h1>About</h1>
+        <Grid container style={{padding:"20px"}}>
+    <p>The  Goal  of  this  project  is  design  an  application  forNational College of Ireland students, 
+        who are lookingfor a place to stay with their peers or alumni. When a studenttries to search for a 
+        place in Dublin, most of the time they gethit with an exorbitant amount of rental fee by property owners.
+        Then  there  are  public  websites  like  daft.ie  and  many  otherwhich provide housing details but don’t 
+        cater to the studentsrequirement. So we have designed the web application to helpanyone find an accommodation, 
+        who is seeking a place to stayin the heart of Dublin and nearby NCI. We have designed theapplication  to  be  
+        easy  to  use  and  simple  enough  to  navigatefind  what  the  student  wants.  Also  this  application  helps  
+        theuser  to  find  a  room  mate  who  is  studying  in  NCI  like  analumni or student who is pursuing the degree. 
+        This also helpspeople  to  find  a  place  near  by  NCI  before  they  even  arrivein Dublin, this especially helps 
+        international students.The webapplication  helps  the  user  to  find  any  vacant  places  hostedby  the  student  
+        who  already  has  a  extra  room  to  share  or  anindividual who can post their property for renting.</p>
+    </Grid>
+        </Grid>)
 }
--- src/components/contact_us/ContactUs.jsx
+++ src/components/contact_us/ContactUs.jsx
@@ -1,3 +1,18 @@
+import { Grid } from "@material-ui/core";
+
 export default function ContactUs(){
-    return(<h1>Contact Us</h1>)
+    return(
+    <Grid>
+    <h1>Contact Us</h1>
+    <Grid>
+    <p>Sathish Kumar Krisnamoorthy: </p> <h3>x20208057@student.ncirl.ie</h3>
+    </Grid>
+    <Grid>
+    <p>James Rusputin Gnanadoss: </p> <h3>x20227990@student.ncirl.ie</h3>
+    </Grid> 
+    <Grid>
+    <p>Sibel Kusak: </p> <h3>x21116202@student.ncirl.ie</h3>
+    </Grid>
+    </Grid>
+    )
 }
